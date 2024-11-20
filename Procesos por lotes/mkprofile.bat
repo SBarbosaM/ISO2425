436e379a99b6c321eee9e7585bd038c3766bf1db
Sergi Barbosa Muñoz <SERGIBM.YAMAHA@GMAIL.COM>
2024-11-20 10:20:02 +0100

    Delete Procesos por lotes/mkprofile.bat

--- Procesos por lotes/mkprofile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Script mkprofile.bat
-REM Autor: Sergi Barbosa Muñoz
-REM Fecha:13/11/2024
-mkdir %USERPROFILE%\asir %USERPROFILE%\daw %USERPROFILE%\ceti %USERPROFILE%\smr
-mkdir %USERPROFILE%\asir\doc  %USERPROFILE%\asir\pract %USERPROFILE%\asir\examen
-mkdir %USERPROFILE%\daw\doc  %USERPROFILE%\daw\pract %USERPROFILE%\daw\examen
-mkdir %USERPROFILE%\ceti\doc  %USERPROFILE%\ceti\pract %USERPROFILE%\ceti\examen
-mkdir %USERPROFILE%\smr\doc  %USERPROFILE%\smr\pract %USERPROFILE%\smr\examen
