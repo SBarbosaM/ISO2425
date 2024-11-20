0a6a41bca9639d55ae3039d2a33740f082b2e113
Sergi Barbosa Muñoz <SERGIBM.YAMAHA@GMAIL.COM>
2024-11-20 10:49:13 +0100

    Add files via upload

---
+++ Procesos por lotes/HolaMundo.bat
@@ -0,0 +1,28 @@
+@echo off
+REM Script mkprofile.bat
+REM Autor: Sergi Barbosa Muñoz
+REM Fecha:13/11/2024
+
+set /P eleccion="Menu: [(1)Crear archivo .txt] [(2)Crear archivo .bat]"
+
+if %eleccion% == 1 goto text
+if %eleccion% == 2 goto bat
+if %eleccion% NEQ 1 goto err
+if %eleccion% NEQ 2 goto err
+
+:text
+set /p nombre="Introduce el nombre del archivo .txt: "
+echo > "%nombre%.txt"
+pause
+exit
+
+:bat
+set /p nombre1="Introduce el nombre del archivo .bat: "
+echo > "%nombre1%.bat"
+pause
+exit
+
+:err
+echo ERROR
+pause
+exit
